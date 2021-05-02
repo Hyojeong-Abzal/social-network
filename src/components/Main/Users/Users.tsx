75418c85b22f389f00eb8c15c19e7d84159d3347
suan <abzal008@mail.ru>
2021-05-02 17:45:43 +0300

    add class component and request to api by componentDidMount

--- src/components/Main/Users/Users.tsx
+++ src/components/Main/Users/Users.tsx
@@ -1,6 +1,7 @@
 import * as axios from "axios";
 import React, { ChangeEvent } from "react";
 import { UsersPageType, UsersType } from "../../../redux/UserReducer";
+import userPhoto from "../../../assets/images/user.png"
 
 type UserPropsType = {
     state: UsersPageType
@@ -9,49 +10,45 @@ type UserPropsType = {
     updateUsers: (users: Array<UsersType>) => void
 }
 
+export class Users extends React.Component<UserPropsType> {
 
-//"https://social-network.samuraijs.com/api/1.0/users"
-export const Users = (props: UserPropsType) => {
-    let getUsers = () => {
-        debugger
-        if (props.state.users.length === 0) {
-            axios.default.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                props.updateUsers(response.data.items)
-            })
-        }
+    componentDidMount() {
+        alert("I know I am inside the DOM")
+        axios.default.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.updateUsers(response.data.items)
+        })
     }
-    return (
+    render() {
+        return (
 
-        <div>
-            <button onClick={getUsers}> Get Users</button>
-            {
-                props.state.users.map(users =>
-                    <div key={users.id}>
-                        <div>
-                            <img src={users.userPhoto} />
+            <div>
+                {
+                    this.props.state.users.map(users =>
+                        <div key={users.id}>
+                            <div>
+                                <img src={userPhoto} />
 
-                            {users.name}
+                                {users.name}
+                            </div>
+                            <div>
+                            </div>
+                            <div>
+                                {users.status}
+
+
+                            </div>
+                            <div>
+                                {users.followed
+                                    ? <button onClick={() => this.props.onUnfollow(users.id)}>Unfollow</button>
+                                    : <button onClick={() => this.props.onFollow(users.id)}>Follow</button>}
+
+
+                            </div>
+
                         </div>
-                        <div>
-                        </div>
-                        <div>
-                            {users.status}
-
-
-                        </div>
-                        <div>
-                            {users.followed
-                                ? <button onClick={() => props.onUnfollow(users.id)}>Unfollow</button>
-                                : <button onClick={() => props.onFollow(users.id)}>Follow</button>}
-
-
-                        </div>
-
-                    </div>
-                )
-            }
-        </div>
-    )
+                    )
+                }
+            </div>
+        )
+    }
 }
-
-
